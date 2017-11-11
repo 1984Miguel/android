e64137e0d6bd389bf089e8065b8006cb6aa7c415
Miguel Mingoarranz Perez <soymingo@gmail.com>
2017-11-11 20:08:08 +0100

    botones mostrar consola

--- app/src/main/java/com/keepcoding/gueder/MainActivity.kt
+++ app/src/main/java/com/keepcoding/gueder/MainActivity.kt
@@ -3,9 +3,18 @@ package com.keepcoding.gueder
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
 
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+
+    //var stone: Button? = null
+    //var burro: Button? = null
+
+
 
     val TAG = MainActivity::class.java.canonicalName
 
@@ -14,9 +23,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var stone =findViewById<Button>(R.id.stone).setOnClickListener(this)
+        var burro = findViewById<Button>(R.id.burro).setOnClickListener(this)
+
+      
+
+
         Log.v(TAG, "He pasado por oncreate")
 
     }
+
+    //TODO: somos los pendejos
+
 
     override fun onSaveInstanceState(outState: Bundle?) {
 
@@ -32,6 +51,47 @@ class MainActivity : AppCompatActivity() {
         outState?.putString("clave","valor")
 
     }
+
+    override fun onClick(v: View?) {
+       Log.v(TAG, "hemos pulsado un boton")
+
+        /*
+        if(v != null)
+        {
+            if (v.getId() == R.id.stone) {
+                Log.v(TAG, "hemos pulsado piedra")
+            } else {
+                Log.v(TAG, "hemos pulsado burro")
+            }
+        }
+
+
+
+        when(v?.id)
+        {
+            R.id.stone -> {
+var a =5
+                var c =7
+                Log.v(TAG, "hemos pulsado piedra")
+
+            }
+            R.id.burro -> Log.v(TAG, "hemos pulsado burro")
+            else ->Log.v(TAG, "ERROR")
+
+        }
+
+        */
+
+
+        Log.v(TAG,when (v?.id){
+            R.id.stone -> "hemos pulsado piedra"
+            R.id.burro -> "hemos pulsado burro"
+            else -> "ERROR"
+
+        })
+
+    }
+
 
 
 }
