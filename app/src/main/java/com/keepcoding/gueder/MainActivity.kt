a61d6205b541a2340864e06f120f4ac91328627e
Miguel Mingoarranz Perez <soymingo@gmail.com>
2017-11-11 16:40:14 +0100

    primeros cambios log

--- app/src/main/java/com/keepcoding/gueder/MainActivity.kt
+++ app/src/main/java/com/keepcoding/gueder/MainActivity.kt
@@ -9,13 +9,29 @@ class MainActivity : AppCompatActivity() {
 
     val TAG = MainActivity::class.java.canonicalName
 
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.v(TAG, "He pasado por oncreate")
+
     }
 
+    override fun onSaveInstanceState(outState: Bundle?) {
 
+    super.onSaveInstanceState(outState)
+        Log.v(TAG, "paso por errr save" )
+
+ //       if (outState != null)
+ //       {
+ //           outState.putString("clave","valor")
+ //       }
+        
+
+        outState?.putString("clave","valor")
+
+    }
 
 
 }
